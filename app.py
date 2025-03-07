ee75639c326750e3ea87264fbb93bffa3170bc65
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-03-07 03:48:56 +0100

    add caching

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel
+from diskcache import Cache
 
 app = FastAPI()
 model = SentenceTransformer('all-MiniLM-L6-v2')  # 384-dimensional embeddings
+cache = Cache("cachedir")
 
 class EmbeddingInput(BaseModel):
     text: str
 
 @app.post("/embed")
 async def generate_embedding(body:EmbeddingInput):
-    embedding = model.encode(body.text, convert_to_tensor=False).tolist()
+    embedding = generate_embedding(body.text)
     return {"embedding": embedding}
+
+#cache is useful because the generated posts are often repeated
+@cache.memoize()
+def generate_embedding(text):
+    return model.encode(text, convert_to_tensor=False).tolist()
