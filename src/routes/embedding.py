324f68a4f85443e6f70adc8bc383f9e3dc8cd181
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-03-26 00:56:38 +0100

    remove caching

--- src/routes/embedding.py
+++ src/routes/embedding.py
@@ -1,22 +1,19 @@
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel
-from diskcache import Cache
 from fastapi import APIRouter
 
 router = APIRouter()
 
 model = SentenceTransformer('all-MiniLM-L6-v2')  # 384-dimensional embeddings
-cache = Cache(".cache")
 
 class EmbeddingInput(BaseModel):
     text: str
 
 @router.post("/embedding")
-async def generate_embedding(body:EmbeddingInput):
+async def generate_embedding_route(body:EmbeddingInput):
+    #todo: add url query for caching
     embedding = generate_embedding(body.text)
     return {"embedding": embedding}
 
-#cache is useful because the generated posts are often repeated
-@cache.memoize()
 def generate_embedding(text):
     return model.encode(text, convert_to_tensor=False).tolist()
