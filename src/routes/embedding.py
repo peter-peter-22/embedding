d2dc90caf4ffcc3a857752f9178a0cdad1d30a85
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-04-12 23:03:39 +0200

    revert

--- src/routes/embedding.py
+++ src/routes/embedding.py
@@ -10,13 +10,10 @@ model = SentenceTransformer('all-MiniLM-L6-v2')  # 384-dimensional embeddings
 class EmbeddingInput(BaseModel):
     text: str
 
-sem = asyncio.Semaphore(4)
-
 @router.post("/embedding")
 async def generate_embedding_route(body:EmbeddingInput):
-    async with sem:
-        embedding = await asyncio.to_thread(generate_embedding,body.text)
-        return {"embedding": embedding}
+    embedding = await asyncio.to_thread(generate_embedding,body.text)
+    return {"embedding": embedding}
 
 def generate_embedding(text):
     return model.encode(text, convert_to_tensor=False).tolist()
