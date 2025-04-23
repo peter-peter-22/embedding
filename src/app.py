b24cb7a306967e7d1a160a6e41623237d48f405c
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-04-23 04:29:37 +0200

    add tests

--- src/app.py
+++ src/app.py
@@ -7,6 +7,8 @@ from src.routes.embedding import router as embedding_router
 from src.routes.home import router as home_router
 from src.routes.graph_clustering import router as clustering_router
 from src.routes.visualize_clusters import router as visualize_clusters_router
+from src.routes.stress_test import router as stress_test_router
+from src.routes.batch_size_test import router as batch_test
 
 app = FastAPI()
 
@@ -15,3 +17,5 @@ app.include_router(embedding_router)
 app.include_router(home_router)
 app.include_router(clustering_router)
 app.include_router(visualize_clusters_router)
+app.include_router(stress_test_router)
+app.include_router(batch_test)
--- src/routes/embedding.py
+++ src/routes/embedding.py
@@ -1,19 +1,22 @@
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel
 from fastapi import APIRouter
+import torch
 
 router = APIRouter()
 
-model = SentenceTransformer('all-MiniLM-L6-v2')  # 384-dimensional embeddings
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = SentenceTransformer('all-MiniLM-L6-v2',device=device)  # 384-dimensional embeddings
 
 class EmbeddingInput(BaseModel):
     text: str
 
 @router.post("/embedding")
 def generate_embedding_route(body:EmbeddingInput):
+    # TODO: 10k empty requests take 13s to process, while 10k embeddings take 7s on GPU, or 33s on CPU (1 thread). Creating the batches on the nodejs server would solve this.
     embedding = generate_embedding(body.text)
     return {"embedding": embedding}
-    
 
 def generate_embedding(text):
     return model.encode(text, convert_to_tensor=False).tolist()
